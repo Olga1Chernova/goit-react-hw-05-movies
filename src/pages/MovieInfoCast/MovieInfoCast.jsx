e0ce77182c822520db170f089bc52446e4501375
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-03-02 09:46:12 +0200

    Update MovieInfoCast.jsx

--- src/pages/MovieInfoCast/MovieInfoCast.jsx
+++ src/pages/MovieInfoCast/MovieInfoCast.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 
 import { movieCast } from 'shared/services/movies-api';
 
